1cf4f99555a503f2d0cd1da5febf01532e1a9830
David Hutchison <dhutchis68516@gmail.com>
2020-05-15 14:49:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,10 +40,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name
-      return self.find_by_name
+    if self.find_by_name(name)
+      return self.find_by_name(name)
     else
-      return self.create_by_name
+      return self.create_by_name(name)
     end
   end
 end
